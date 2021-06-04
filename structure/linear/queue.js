a8ba6960b33b9fe39a7685113bf3d4fc920a1e28
kiJu2 <thdydehfdl12@gmail.com>
2021-06-04 15:17:44 +0900

    init

---
+++ structure/linear/queue.js
@@ -0,0 +1,20 @@
+function queue(){
+  function node(value, next){
+    return{
+      value : value,
+      next : next,
+    };
+  }
+
+  this.isEmpty = function(){
+    
+  }
+  this.push = function(){
+
+  }
+  this.pop = function(){
+
+  }
+}
+
+queue()
